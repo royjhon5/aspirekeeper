bed3c0e4a4db54f7ce48dc9d6781505ff3c93b76
DESKTOP-VLTCHR6\Roy Developer <rjisraelcubillan55@gmail.com>
2025-03-26 20:27:57 +0800

    update: latest update

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,6 +21,7 @@ export const metadata: Metadata = {
     "Aspire Keeper handles your tasks and to-do lists, so you can focus on achieving your goals. We offer virtual assistance to streamline work and boost efficiency.",
   robots: "index, follow",
   keywords: [
+    "aspire keeper",
     "virtual assistant",
     "productivity",
     "task management",
@@ -29,7 +30,7 @@ export const metadata: Metadata = {
     "AI assistant",
   ],
   openGraph: {
-    url: "https://aspirekeeper.vercel.app/",
+    url: "https://aspirekeeper.com/",
     type: "website",
     title: "Aspire Keeper Solutions",
     description:
@@ -37,7 +38,7 @@ export const metadata: Metadata = {
     siteName: "Aspire Keeper Solutions",
     images: [
       {
-        url: "https://aspirekeeper.vercel.app/thumbnail/thumbnail.png",
+        url: "https://aspirekeeper.com/thumbnail/thumbnail.png",
         width: 1200,
         height: 630,
         alt: "Aspire Keeper Solutions Thumbnail",
@@ -53,7 +54,7 @@ export const metadata: Metadata = {
     site: "@rjdev",
     images: [
       {
-        url: "https://aspirekeeper.vercel.app/thumbnail/thumbnail.png",
+        url: "https://aspirekeeper.com/thumbnail/thumbnail.png",
         width: 1200,
         height: 630,
         alt: "Aspire Keeper Thumbnail",
@@ -61,7 +62,7 @@ export const metadata: Metadata = {
     ],
   },
   alternates: {
-    canonical: "https://aspirekeeper.vercel.app/",
+    canonical: "https://aspirekeeper.com/",
   },
 };
 
@@ -74,18 +75,18 @@ export default function RootLayout({
         <meta name="google-site-verification" content="n2nGFT_84o6GIzBOBmVYbeu42xfdaRrpsit-llx3vNo" />
         
         {/* Favicons */}
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="icon" type="image/x-icon" href="https://aspirekeeper.vercel.app/favicon.ico" sizes="32x32" />
+        <link rel="icon" type="image/png" sizes="32x32" href="https://aspirekeeper.com/favicon/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="96x96" href="https://aspirekeeper.com/favicon/favicon-96x96.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="https://aspirekeeper.com/favicon/favicon-16x16.png" />
+        <link rel="icon" type="image/x-icon" href="https://aspirekeeper.com/favicon/favicon.ico" sizes="32x32" />
         <link rel="manifest" href="/manifest.json" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
         
         {/* Apple Touch Icons */}
-        <link rel="apple-touch-icon" sizes="152x152" href="/apple-icon-152x152.png" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="apple-touch-icon" sizes="152x152" href="https://aspirekeeper.com/apple-icon-152x152.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="https://aspirekeeper.com/apple-touch-icon.png" />
         <meta name="msapplication-TileColor" content="#ffffff" />
-        <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
+        <meta name="msapplication-TileImage" content="https://aspirekeeper.com/ms-icon-144x144.png" />
         <meta name="theme-color" content="#ffffff" />
         <link rel="manifest" href="/site.webmanifest" />
 
@@ -97,7 +98,7 @@ export default function RootLayout({
               "@context": "https://schema.org",
               "@type": "WebSite",
               "name": "Aspire Keeper Solutions",
-              "url": "https://aspirekeeper.vercel.app/",
+              "url": "https://aspirekeeper.com/",
               "description":
                 "Aspire Keeper handles your tasks and to-do lists, so you can focus on achieving your goals. We offer virtual assistance to streamline work and boost efficiency.",
               "publisher": {
@@ -105,13 +106,13 @@ export default function RootLayout({
                 "name": "Aspire Keeper",
                 "logo": {
                   "@type": "ImageObject",
-                  "url": "https://aspirekeeper.vercel.app/thumbnail/thumbnail.png",
+                  "url": "https://aspirekeeper.com/thumbnail/thumbnail.png",
                 },
               },
-              "image": "https://aspirekeeper.vercel.app/thumbnail/thumbnail.png",
+              "image": "https://aspirekeeper.com/thumbnail/thumbnail.png",
               "potentialAction": {
                 "@type": "SearchAction",
-                "target": "https://aspirekeeper.vercel.app/search?q={search_term}",
+                "target": "https://aspirekeeper.com/search?q={search_term}",
                 "query-input": "required name=search_term",
               },
             }),
